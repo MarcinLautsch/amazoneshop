378826384ceb9bc299fbf3fc367caed185922aab
Marcin Lautsch <mldemigod@gmail.com>
2022-10-07 19:58:25 +0200

    login_fireBase_work

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,37 @@
 import React, { useState } from 'react';
 import './Login.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { auth } from './firebase';
 
 function Login() {
+  const history = useHistory();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const signIn = (event) => {
-    event.preventDefault();
-    //firebase uzywac do logowania//
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push('/');
+      })
+      .catch((error) => alert(error.message));
   };
 
-  cosnt register = (event) => {
-    event.preventDefault();
-        //firebase uzywac do rejestracja//
-  };
+  const register = (e) => {
+    e.preventDefault();
 
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // it successfully created a new user with email and password
+        if (auth) {
+          history.push('/');
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
   return (
     <div className="login">
       <Link to="/">
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,9 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth } from 'firebase/auth';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyCcKyQlFm9cRqTCT0a64lc5D77KnHQkrQs',
   authDomain: 'amozneclone.firebaseapp.com',
@@ -8,3 +13,9 @@ const firebaseConfig = {
   appId: '1:151477503776:web:2af5eb530694107fa1b945',
   measurementId: 'G-F67SPG5G98'
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
